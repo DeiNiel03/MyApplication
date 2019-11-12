e022a0adc3924eee1dbf6f421009475fd2bab369
unknown <Elorrieta Errekamari@INFP4A101-24.H015112.NET>
2019-11-12 14:25:06 +0100

    pantalla de inicio

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences preferencias= getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferencias.edit();
+
+        editor.putString("Eusuario", "admin");
+        editor.putString("Epassword", "admin");
+        editor.commit();
+    }
+
+    public void iniciarSesion(View view){
+
+        SharedPreferences preferencias= getPreferences(MODE_PRIVATE);
+
+
+        EditText Eusuario = (EditText)findViewById(R.id.regisUsuario);
+        EditText Epassword = (EditText)findViewById(R.id.regisPass);
+
+        String usuario = preferencias.getString("Eusuario","");
+        String password = preferencias.getString("Epassword","");
     }
 }
